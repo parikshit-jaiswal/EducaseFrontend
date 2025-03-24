589b1a679f95b4558d74ee4432fdd7184940c766
parikshit-jaiswal <parikshitjaiswal82@gmail.com>
2025-03-24 17:45:42 +0530

    fix: update import paths to use .jsx file extensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import AccountSetting from "./pages/AccountSettingPage.js"
-import LandingPage from "./pages/LandingPage.js"
-import LoginPage from "./pages/loginPage.js"
-import RegisterPage from "./pages/RegisterPage.js"
+import AccountSetting from "./pages/AccountSettingPage.jsx"
+import LandingPage from "./pages/LandingPage.jsx"
+import LoginPage from "./pages/loginPage.jsx"
+import RegisterPage from "./pages/RegisterPage.jsx"
 import { Toaster } from "@/components/ui/sonner"
 
 function App() {
